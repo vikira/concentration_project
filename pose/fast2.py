d091380eddbda07a6c7a1f4b258ad5d8a6a40a83
lgaeun <gaeungaeun4837@gmail.com>
2020-12-08 15:06:05 +0900

    repository 이동

---
+++ pose/fast2.py
@@ -0,0 +1,116 @@
+import cv2
+import time
+import numpy as np
+import argparse
+import math
+
+parser = argparse.ArgumentParser(description='Run keypoint detection')
+parser.add_argument("--device", default="cpu", help="Device to inference on")
+parser.add_argument("--video_file", default="sample_video.mp4", help="Input Video")
+
+args = parser.parse_args()
+
+MODE = "COCO"
+
+if MODE is "COCO":
+    protoFile = "pose_deploy_linevec.prototxt"
+    weightsFile = "pose_iter_440000.caffemodel"
+    nPoints = 18
+    POSE_PAIRS = [ [1,0],[1,2],[1,5],[2,3],[3,4],[5,6],[6,7],[0,14],[0,15],[14,16],[15,17]]
+
+elif MODE is "MPI" :
+    protoFile = "pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt"
+    weightsFile = "pose/mpi/pose_iter_160000.caffemodel"
+    nPoints = 15
+    POSE_PAIRS = [[0,1], [1,2], [2,3], [3,4], [1,5], [5,6], [6,7], [1,14], [14,8], [8,9], [9,10], [14,11], [11,12], [12,13] ]
+
+
+inWidth = 368
+inHeight = 368
+threshold = 0.1
+iter=0
+neck=200
+cap = cv2.VideoCapture(0)
+hasFrame, frame = cap.read()
+
+vid_writer = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame.shape[1],frame.shape[0]))
+
+net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
+if args.device == "cpu":
+    net.setPreferableBackend(cv2.dnn.DNN_TARGET_CPU)
+    print("Using CPU device")
+elif args.device == "gpu":
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+    print("Using GPU device")
+
+while cv2.waitKey(1) < 0:
+    t = time.time()
+    hasFrame, frame = cap.read()
+    frameCopy = np.copy(frame)
+    if not hasFrame:
+        cv2.waitKey()
+        break
+
+    frameWidth = frame.shape[1]
+    frameHeight = frame.shape[0]
+
+    inpBlob = cv2.dnn.blobFromImage(cv2.resize(frame,(300,300)), 1.0 / 255, (300,300),
+                              (104.0,177.0, 123.0), swapRB=False, crop=False)
+    net.setInput(inpBlob)
+    output = net.forward()
+
+    H = output.shape[2]
+    W = output.shape[3]
+    # Empty list to store the detected keypoints
+    points = []
+    d =[]
+    for i in range(nPoints):
+        # confidence map of corresponding body's part.
+        probMap = output[0, i, :, :]
+
+        # Find global maxima of the probMap.
+        minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
+        
+        # Scale the point to fit on the original image
+        x = (frameWidth * point[0]) / W
+        y = (frameHeight * point[1]) / H
+
+        if prob > threshold : 
+            cv2.circle(frameCopy, (int(x), int(y)), 8, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
+            cv2.putText(frameCopy, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, lineType=cv2.LINE_AA)
+
+            # Add the point to the list if the probability is greater than the threshold
+            points.append((int(x), int(y)))
+        else :
+            points.append(None)
+
+    # Draw Skeleton
+    for pair in POSE_PAIRS:
+        partA = pair[0]
+        partB = pair[1]
+
+        if points[partA] and points[partB]:
+            cv2.line(frame, points[partA], points[partB], (0, 255, 255), 3, lineType=cv2.LINE_AA)
+            cv2.circle(frame, points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
+            cv2.circle(frame, points[partB], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
+    
+    if points[0] and points[1]:
+        d.append(points[0][0]-points[1][0])
+        d.append(points[0][1]-points[1][1])
+        neck = math.sqrt(d[0]*d[0]+d[1]*d[1])
+        print(neck)
+        #iter++
+        #avg = (avg +neck) / iter
+    if neck > 200:
+        cv2.putText(frame, "head up", (50, 100), cv2.FONT_HERSHEY_COMPLEX, .6, (255, 50, 0), 2, lineType=cv2.LINE_AA)
+    if neck < 140:
+        cv2.putText(frame, "head down", (50, 100), cv2.FONT_HERSHEY_COMPLEX, .6, (255, 50, 0), 2, lineType=cv2.LINE_AA)
+    cv2.putText(frame, "time taken = {:.2f} sec".format(time.time() - t), (50, 50), cv2.FONT_HERSHEY_COMPLEX, .6, (255, 50, 0), 2, lineType=cv2.LINE_AA)
+    # cv2.putText(frame, "OpenPose using OpenCV", (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 50, 0), 2, lineType=cv2.LINE_AA)
+    # cv2.imshow('Output-Keypoints', frameCopy)
+    cv2.imshow('Output-Skeleton', frame)
+
+    vid_writer.write(frame)
+
+vid_writer.release()
